88e31b6504fa5039ff5ccaec7bed87adf3587a91
nob-islan <nob-islan@users.noreply.github.com>
2026-08-16 06:36:54 +0000

    deploy: d390f6e7cb391b7e573496c1ee108c47e34d3114

---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.D4p_vs5l.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DKoP6XSi.js";import{_ as a}from"../app.Ciu9mcAm.js";import"./chunk-5VM5RSS4.CwyjJrWF.js";import"./chunk-XXDRQBXY.BskHCxg8.js";import"./chunk-KBJHAD2P.BehW8xvd.js";import"./chunk-2GRJ4B5K.Bt6eH61D.js";import"./channel.A2yg92Og.js";import"./framework.C05ZgNLH.js";import"./theme.DhYj6AAk.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
