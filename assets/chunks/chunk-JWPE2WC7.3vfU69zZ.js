0ed38888a70fac0c7aba75a4e5a7a5af829a110b
nob-islan <nob-islan@users.noreply.github.com>
2026-08-30 07:29:38 +0000

    deploy: c186d1ec6715556e0ee38628fa35707e90a1a356

--- assets/chunks/chunk-JWPE2WC7.3vfU69zZ.js
+++
@@ -1,1 +0,0 @@
-import{_ as i}from"../app.CU70iIek.js";function t(c,e){c.accDescr&&e.setAccDescription?.(c.accDescr),c.accTitle&&e.setAccTitle?.(c.accTitle),c.title&&e.setDiagramTitle?.(c.title)}i(t,"populateCommonDb");export{t as p};
---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.CPXv6_SW.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.CvkOniES.js";import{_ as a}from"../app.Dk-8zsVt.js";import"./chunk-5VM5RSS4.CSObrvHU.js";import"./chunk-XXDRQBXY.pAa3sGgu.js";import"./chunk-KBJHAD2P.0a_tkSvE.js";import"./chunk-2GRJ4B5K.DjCijKXO.js";import"./channel.CKw9JL3S.js";import"./framework.C05ZgNLH.js";import"./theme.BBpxdaDy.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
