b412dc5cbbec40939fe5214dc57a219824e8b97f
nob-islan <nob-islan@users.noreply.github.com>
2026-09-01 15:04:23 +0000

    deploy: 464f3ec0e5a49fece1ac1d0a5abd3f5291b3277c

--- assets/chunks/channel.Bzxh9aNX.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.DTyPRkWX.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
---
+++ assets/chunks/swimlanesDiagram-ULZ7WXOC.qJ0nw6Kj.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.BtVHZrLL.js";import{_ as a}from"../app.BcYoJCrb.js";import"./chunk-5VM5RSS4.BTQVj9Gv.js";import"./chunk-XXDRQBXY.D9CW_SuR.js";import"./chunk-KBJHAD2P.BPDTYCJl.js";import"./chunk-2GRJ4B5K.DwXag5k6.js";import"./channel.CQskXjQo.js";import"./framework.C05ZgNLH.js";import"./theme.DVZlbSll.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
