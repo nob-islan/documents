69d432831476173ad9005a1b2dca7e29e8872e48
nob-islan <nob-islan@users.noreply.github.com>
2026-09-05 02:11:30 +0000

    deploy: e6b482194c524a0bf711de4277688eeaa2c66ba4

--- assets/chunks/swimlanesDiagram-ULZ7WXOC.BIM__U9t.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DsUFyamc.js";import{_ as a}from"../app.BOrecACs.js";import"./chunk-5VM5RSS4.IMDd5Ldq.js";import"./chunk-XXDRQBXY.B4RzQe2V.js";import"./chunk-KBJHAD2P.CcYra-qj.js";import"./chunk-2GRJ4B5K.CUpaSro-.js";import"./channel.G3jxvXpr.js";import"./framework.C05ZgNLH.js";import"./theme.B7xUon_M.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
